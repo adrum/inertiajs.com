94c7abf95ae45afedcd9a7e78dcd923c816311a7
Dominik <puRe1337@users.noreply.github.com>
2025-09-25 11:18:27 +0200

    docs: unify variable naming in error handling example (#460)

--- resources/js/Pages/error-handling.jsx
+++ resources/js/Pages/error-handling.jsx
@@ -179,14 +179,14 @@ export default function () {
               <script>
                 let { status } = $props()
 
-                const titles = {
+                const title = {
                   503: '503: Service Unavailable',
                   500: '500: Server Error',
                   404: '404: Page Not Found',
                   403: '403: Forbidden',
                 }
 
-                const descriptions = {
+                const description = {
                   503: 'Sorry, we are doing some maintenance. Please check back soon.',
                   500: 'Whoops, something went wrong on our servers.',
                   404: 'Sorry, the page you are looking for could not be found.',
@@ -195,7 +195,7 @@ export default function () {
               </script>
 
               <div>
-                <h1>{titles[status]}</h1>
+                <h1>{title[status]}</h1>
                 <div>{description[status]}</div>
               </div>
             `,
